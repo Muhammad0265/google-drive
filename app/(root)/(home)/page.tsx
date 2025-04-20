cc3bcd9ff64070f5c2817c0f58b4d5c3a850348b
muhammadjon <shuxrat0166@gmail.com>
2025-04-20 20:38:46 +0500

    again fixed php

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,7 @@
 
 import { UserButton } from '@clerk/nextjs'
-import { getUser } from "@/actions/auth-action"
 
 const Page = async () => {
-  const data = await getUser()
-  console.log(data);
   return (
     <div>
       <UserButton />
