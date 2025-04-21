0f92ccbb053111a76e9dc2ca3b7099d0fffef381
muhammadjon <shuxrat0166@gmail.com>
2025-04-21 12:24:52 +0500

    again fixed network

--- actions/auth-action.ts
+++ actions/auth-action.ts
@@ -3,23 +3,18 @@
 import { axiosClient } from '@/http/axios'
 import { UserCreate } from './type'
 
-export const createUser = async ({
-	clerkId,
-	email,
-	fullName,
-	picture,
-}: UserCreate) => {
-	try {
-		const { data } = await axiosClient.post('/api/user/media', {
-			email,
-			fullName,
-			clerkId,
-			picture,
-		})
-		return data
-	} catch (err) {
-		throw new Error(`Error creating user ${err}`)
+export async function createUser(data: UserCreate) {
+	const res = await fetch('http://127.0.0.1:8500/api/user/media', {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: { 'Content-Type': 'application/ld+json' },
+	})
+
+	if (!res.ok) {
+		throw new Error('Error creating user')
 	}
+
+	return await res.json()
 }
 
 export const getUser = async () => {
